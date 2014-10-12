3d3287b069929f96e5949ed2c4f951c0c21fdf96
haiko <haiko@hotmail.com>
2014-10-12 21:59:03 +0200

    - Copied required helpers from forem. - Added method allowing params needed for devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,43 @@
 class ApplicationController < ActionController::Base
 
-  def forem_user
-    current_user
-  end
-  helper_method :forem_user
+	def forem_user
+		current_user
+	end
+	helper_method :forem_user
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Copied from Forem::ApplicationController, because helper was not available here.
+	def forem_admin?
+		forem_user && forem_user.forem_admin?
+	end
+	helper_method :forem_admin?
 
-  before_action :authenticate_user!
+	# Copied from Forem::ApplicationController, because helper was not available here.
+	def forem_admin_or_moderator?(forum)
+		forem_user && (forem_user.forem_admin? || forum.moderator?(forem_user))
+	end
+	helper_method :forem_admin_or_moderator?
+
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	before_action :authenticate_user!
+
+	before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_devise_permitted_parameters
+		registration_params = [:name, :email, :password, :password_confirmation]
+
+		if params[:action] == 'update'
+			devise_parameter_sanitizer.for(:account_update) {
+					|u| u.permit(registration_params << :current_password, {roles: []})
+			}
+		elsif params[:action] == 'create'
+			devise_parameter_sanitizer.for(:sign_up) {
+					|u| u.permit(registration_params)
+			}
+		end
+	end
 end
