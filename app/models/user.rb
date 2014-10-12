dfa983361bb7c0d645911143531d6bbc991acbb3
haiko <haiko@hotmail.com>
2014-10-12 22:01:01 +0200

    - Added roles to user. - Added validation for name. - Implemented forem_admin? properly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,32 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	ROLES = %w[admin moderator banned]
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	       :recoverable, :rememberable, :trackable, :validatable
 
-  # FIXME change this in an actual name
-  def forem_name
-	  email
-  end
+	validates :name, presence: true
+	validates :name, uniqueness: true
 
-  def forem_email
-	  email
-  end
+	def forem_name
+		name
+	end
+
+	def forem_email
+		email
+	end
 
 	def forem_admin?
-		email.match /haikobleumink/
+		roles.include? 'admin'
+	end
+
+	def roles=(roles)
+		self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+	end
+
+	def roles
+		ROLES.reject do |r|
+			((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+		end
 	end
 end
