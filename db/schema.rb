688cd5604958a5283669fc19f91a54047253b13f
haiko <haiko@hotmail.com>
2014-10-13 20:34:46 +0200

    Updated schema for roles and name.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002213935) do
+ActiveRecord::Schema.define(version: 20141006210622) do
 
   create_table "forem_categories", force: true do |t|
     t.string   "name",       null: false
@@ -124,6 +124,8 @@ ActiveRecord::Schema.define(version: 20141002213935) do
     t.boolean  "forem_admin",            default: false
     t.string   "forem_state",            default: "pending_review"
     t.boolean  "forem_auto_subscribe",   default: false
+    t.string   "name"
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
