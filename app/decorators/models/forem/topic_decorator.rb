6cebe0ffbc6d5aaf3468a91cd47f40382fe1557d
haiko <haiko@hotmail.com>
2014-10-19 13:58:41 +0200

    Topics can be deleted by user x if all posts are by the user x.

--- app/decorators/models/forem/topic_decorator.rb
+++ app/decorators/models/forem/topic_decorator.rb
@@ -1,6 +1,6 @@
 Forem::Topic.class_eval do
 	# Check whether a topic may be deleted.
 	def can_be_deleted?
-		forem_user.forem_admin? || (self.posts.map { |post| post.user == forem_user } == [true])
+		forem_user.forem_admin? || (self.posts.map { |post| post.user == forem_user }.uniq == [true])
 	end
 end
